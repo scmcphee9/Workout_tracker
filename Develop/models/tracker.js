4c9d7b2eb814120e1c0b3a538d9087c9c5df2fe2
Shawn McPhee <scmcphee9@gmail.com>
2021-02-03 20:38:50 -0700

    updated server, and created a schema in the models folder

--- Develop/models/tracker.js
+++ Develop/models/tracker.js
@@ -1,1 +1,36 @@
 // build schema
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+  day: { type: Date, default: Date.now },
+  exercise: [
+    {
+      type: { type: String, required: true, trim: true },
+      name: { type: String, trim: true, required: true },
+      duration: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
+      sets: {
+        type: Number,
+        unique: true,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Workout = mongoos.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,29 @@
 // build express server
+const express = require("express");
+const mongoose = require("mongoos");
+const logger = require("morgan");
+// const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
