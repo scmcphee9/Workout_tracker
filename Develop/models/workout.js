6993940775215114da23a197650fdd5e4985bec3
Shawn McPhee <scmcphee9@gmail.com>
2021-02-07 15:55:45 -0700

    cleaned up comments and code

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -8,31 +8,31 @@ const workoutSchema = new mongoose.Schema(
     day: { type: Date, default: Date.now },
     exercises: [
       {
-        type: { type: String, trim: true },
-        name: { type: String, trim: true },
+        type: {
+          type: String,
+          trim: true,
+          required: "You must enter the type of the workout.",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "You must enter the name of the workout.",
+        },
         duration: {
           type: Number,
-          // unique: true,
-          // required: true,
+          required: "You must enter the duration of the workout.",
         },
         weight: {
           type: Number,
-          // unique: true,
-          // required: true,
         },
         reps: {
           type: Number,
-          // unique: true,
-          // required: true,
         },
         sets: {
           type: Number,
-          // unique: true,
-          // required: true,
         },
         distance: {
           type: Number,
-          // unique: true,
         },
       },
     ],
