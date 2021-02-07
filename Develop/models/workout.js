5ae8348e2c6234b24767000bba4227a633d76985
Shawn McPhee <68212252+scmcphee9@users.noreply.github.com>
2021-02-07 15:35:30 -0700

    updated workout.js to add virtuals to fix total duration

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -1,38 +1,49 @@
 // build schema
 const mongoose = require("mongoose");
 
-const workoutSchema = new mongoose.Schema({
-  day: { type: Date, default: Date.now },
-  exercises: [
-    {
-      type: { type: String, trim: true },
-      name: { type: String, trim: true },
-      duration: {
-        type: Number,
-        // unique: true,
-        // required: true,
+const virtual = { toJSON: { virtuals: true } };
+
+const workoutSchema = new mongoose.Schema(
+  {
+    day: { type: Date, default: Date.now },
+    exercises: [
+      {
+        type: { type: String, trim: true },
+        name: { type: String, trim: true },
+        duration: {
+          type: Number,
+          // unique: true,
+          // required: true,
+        },
+        weight: {
+          type: Number,
+          // unique: true,
+          // required: true,
+        },
+        reps: {
+          type: Number,
+          // unique: true,
+          // required: true,
+        },
+        sets: {
+          type: Number,
+          // unique: true,
+          // required: true,
+        },
+        distance: {
+          type: Number,
+          // unique: true,
+        },
       },
-      weight: {
-        type: Number,
-        // unique: true,
-        // required: true,
-      },
-      reps: {
-        type: Number,
-        // unique: true,
-        // required: true,
-      },
-      sets: {
-        type: Number,
-        // unique: true,
-        // required: true,
-      },
-      distance: {
-        type: Number,
-        // unique: true,
-      },
-    },
-  ],
+    ],
+  },
+  virtual
+);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workouts = mongoose.model("Workout", workoutSchema);
